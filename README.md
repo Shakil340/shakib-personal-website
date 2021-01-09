3c5f7a55cfb24a2c514b9464a01ce6a8fd49b3f7
Shakil340 <ashakil340@gmail.com>
2021-01-09 20:36:48 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# shakib-personal-website
+ 
---
+++ js/custom.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('.dropdown-menu2').hide();
+    $('#navbarDropdown').click(function () {
+        $('ul.dropdown-menu2 ').toggle();
+
+    });
+    $('.dropdown-menu3').hide();
+    $('#navbardropdown').click(function () {
+        $(' .dropdown-menu3').toggle();
+
+    });    
+    
+
+});
